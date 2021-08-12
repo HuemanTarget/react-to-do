de63e7190586e1a7a98824f729f08886804b5bb1
HuemanTarget <joshbasche@live.com>
2021-08-12 14:53:33 -0700

    create NewSkillForm component

---
+++ src/components/NewSkillForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NewSkillForm() {
+  return(
+    <div>
+      <form>
+        <h1>Hello</h1>
+      </form>
+    </div>
+  )
+}
+
+export default NewSkillForm;
